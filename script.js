c80de327e93f448b09037e44c11fb30c8f923db6
Italo Paixao Gomes <italopaxg@gmail.com>
2022-02-18 23:49:10 -0300

    Consumindo API

---
+++ script.js
@@ -0,0 +1,42 @@
+function createEle (type, conteudo, infos) {
+    let newElement = document.createElement("p");
+    if(type === "name"){
+        let name = conteudo;
+        name = name[0].toUpperCase() + name.substr(1);
+        newElement.innerText = `Name: ${name}`;
+    } else if(type === "height") {
+        if(conteudo < 10){
+            newElement.innerText = `Height: ${conteudo*10}cm`;
+        } else{
+            newElement.innerText = `Height: ${conteudo/10}m`;
+        }
+    } else if(type === "weight") {
+        newElement.innerText = `Weight: ${conteudo}kg`;
+    }
+    infos.appendChild(newElement);
+}
+
+function clean (area) {
+    area.innerHTML = "";
+}
+
+function showInfos () {
+
+    let input = document.querySelector("#valor");
+    let inputValue = input.value;
+    let image = document.querySelector(".image-pokemon")
+    let infos = document.querySelector(".infos")
+    let url = "https://pokeapi.co/api/v2/pokemon/";
+    
+    clean(infos);
+
+    fetch(`${url}${inputValue}`)
+        .then((res)=>{
+            return res.json();
+        }).then((data)=>{
+            image.style.backgroundImage = `url(${data.sprites.other.dream_world.front_default})`;
+            createEle("name", data.name, infos);
+            createEle("height", parseFloat(data.height), infos);
+            createEle("weight", parseFloat(data.weight)/10, infos);
+        })
+}
